a226552c2b1430816f308e62e23e0acfefc70f2d
camilo193 <juanus196@gmail.com>
2022-09-22 00:55:45 -0500

    Adding generics examples

---
+++ Generics/Program.cs
@@ -0,0 +1,22 @@
+﻿using Generics;
+
+//This can't be possible, because string isn't a struct, it's a class
+Response<string> response = new Response<string>();
+
+//Response2 has where T : class
+Response2<string> response2 = new();
+
+//It can't be possible, because string isn't a class, it's a struct
+Response2<bool> response3 = new();
+
+//Response 4 can be struct and class at the same time
+Response4<string> response4 = new();
+Response4<bool> response4_1 = new();
+
+//I define response4 as a string
+response4.Data = "I define this variable as String";
+
+//I define response4_1 as a bool
+response4_1.Data = true;
+
+//Note that response4 and response4_1 belong to the same class and you can define as string or bool
---
+++ Generics/Response.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    /*This is a Class to give responses in the API's
+     * Data would take any type of class, struct or primitive variable
+     * where T : struct, means that T only can be a struct (Primitive variables are structs)
+     * But you can use where T : class. That means that T only can be a class
+     * If you don't define where T, how you imagine, means T can be anything 
+     * You can define a lot of T behaviors: where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>, etc...
+     * You can have two or more differents generics in the same class: Response<T, Y, X, Z>
+    */
+
+    public class Response<T> where T : struct 
+    {
+        //In this particular case, data gonna be a primitive variable
+        public T Data { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public class Response2<T> where T : class
+    {
+        public T Data { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    //You can't combine struct, class, notnull and default in where T :
+    public class Response3<T> where T : struct, class
+    {
+        public T Data { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public class Response4<T>
+    {
+        public T Data { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+    }
+}
